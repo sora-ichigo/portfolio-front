9e11c1af09118360e010b208573ddff6592badd0
igsr5 <i@gsr5.dev>
2022-01-21 22:12:07 +0900

    Add Lato google font

--- src/hello/world.tsx
+++ src/hello/world.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-export const World: React.FC = () => <StyledText>Hello world!!</StyledText>;
+export const World: React.FC = () => (
+  <>
+    <p className="text-3xl font-bold underline font-main">a</p>
+    <StyledText>Hello world!!</StyledText>
+  </>
+);
 
 const StyledText = styled.p`
   color: red;
