33a22999dac037d483c40b70674fc97878c222ac
igsr5 <i@gsr5.dev>
2022-01-21 23:00:29 +0900

    Add color palette

--- src/hello/world.tsx
+++ src/hello/world.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 
 export const World: React.FC = () => (
   <>
-    <p className="text-3xl font-bold underline font-main">a</p>
-    <StyledText>Hello world!!</StyledText>
+    <p className="text-3xl font-bold font-main text-navy">a</p>
+    <p className="text-3xl font-bold font-main text-grey">a</p>
+    <p className="text-3xl font-bold font-main text-grey-1">a</p>
+    <p className="text-3xl font-bold font-main text-grey-2">a</p>
+    <p className="text-3xl font-bold font-main text-grey-3">a</p>
+    <p className="text-3xl font-bold font-main text-grey-4">a</p>
+    <p className="text-3xl font-bold font-main text-blue">a</p>
+    <p className="text-3xl font-bold font-main text-blue-light">a</p>
+    <p className="text-3xl font-bold font-main text-green">a</p>
+    <p className="text-3xl font-bold font-main text-green-link">a</p>
+    <p className="text-3xl font-bold font-main text-red">a</p>
+    <p className="text-3xl font-bold font-main text-yellow">a</p>
+
+    <p className="text-3xl font-bold font-tabTitle">font-tab-title</p>
   </>
 );
-
-const StyledText = styled.p`
-  color: red;
-  font-size: 36px;
-`;
