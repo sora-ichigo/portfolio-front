720de7eb01c95fd21a836afd577d83b12b4f6acb
igsr5 <i@gsr5.dev>
2022-01-29 18:28:09 +0900

    Add GlobalHeading

--- src/about/components/About.tsx
+++ src/about/components/About.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { GlobalHeading } from "../../common/components/GlobalHeading";
+import { FaUserAlt } from "react-icons/fa";
 
 export const About: React.FC = () => {
-  return <div className="py-4 px-8 w-4/5 mx-auto"></div>;
+  return (
+    <div className="mx-auto w-4/5 py-4 px-8">
+      <GlobalHeading text="A LITTLE ABOUT ME" icon={FaUserAlt} />
+    </div>
+  );
 };
---
+++ src/common/components/GlobalHeading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { IconType } from "react-icons";
+
+type Props = { text: string; icon: IconType };
+
+export const GlobalHeading: React.FC<Props> = ({ text, icon }) => (
+  <div className="flex items-center">
+    <div className="mr-3 flex h-12 w-12 items-center justify-center rounded-full border-4 border-grey-1 text-grey-4">
+      {React.createElement(icon)}
+    </div>
+    <h2 className="pr-4 text-xs font-bold tracking-widest">{text}</h2>
+    <span className="flex-1 border-t-4" />
+  </div>
+);
