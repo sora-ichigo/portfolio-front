cfc6fd5c8ea47d7310bd356e4c07aeaa6d62f41f
igsr5 <i@gsr5.dev>
2022-01-29 18:27:56 +0900

    Add react-icons

--- src/header/components/Header.tsx
+++ src/header/components/Header.tsx
@@ -5,25 +5,25 @@ import { useMediaQuery } from "react-responsive";
 export const Header: React.FC = () => {
   const isWindowMd = useMediaQuery({ query: "(min-width: 768px)" });
   return (
-    <header className="text-center md:p-8 sm:pt-6 sm:px-4 sm:pb-1">
+    <header className="text-center sm:px-4 sm:pt-6 sm:pb-1 md:p-8">
       <Image
         src="/profile_icon.png"
         width={isWindowMd ? 160 : 140}
         height={isWindowMd ? 160 : 140}
         alt="Ichigo Sora"
       />
-      <h1 className="text-2xl font-light mt-3.5 leading-snug">Sora Ichigo</h1>
-      <p className="leading-none italic mb-2  px-2.5 py-1.5 bg-yellow-marker inline-block rounded">
+      <h1 className="mt-3.5 text-2xl font-light leading-snug">Sora Ichigo</h1>
+      <p className="mb-2 inline-block rounded  bg-yellow-marker px-2.5 py-1.5 italic leading-none">
         software engineer
       </p>
-      <ul className="flex max-w-5xl mx-auto h-24 items-center">
-        <li className="w-2/6 md:text-4/5xl sm:text-xl font-tabTitle font-bold text-grey-4 cursor-pointer">
+      <ul className="mx-auto flex h-24 max-w-5xl items-center">
+        <li className="w-2/6 cursor-pointer font-tabTitle font-bold text-grey-4 sm:text-xl md:text-4/5xl">
           about me
         </li>
-        <li className="w-2/6 md:text-4/5xl sm:text-xl font-tabTitle font-bold">
+        <li className="w-2/6 font-tabTitle font-bold sm:text-xl md:text-4/5xl">
           portfolio
         </li>
-        <li className="w-2/6 md:text-4/5xl sm:text-xl font-tabTitle font-bold text-grey-4  cursor-pointer">
+        <li className="w-2/6 cursor-pointer font-tabTitle font-bold text-grey-4 sm:text-xl  md:text-4/5xl">
           contact
         </li>
         <li className="hidden">resume</li>
