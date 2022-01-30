f814ffd82b81413c2a003a627ad6f177faa82ca6
igsr5 <i@gsr5.dev>
2022-01-30 18:06:15 +0900

    Add styles/index.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import "../styles/index.css";
 
 const App = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />;
