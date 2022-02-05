41dd369a3fcc5f3199b4e4480c544492b813f519
igsr5 <i@gsr5.dev>
2022-02-06 01:58:04 +0900

    Add pages/404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+
+// TODO: 後でちゃんと実装する
+const Custom404: NextPage = () => {
+  return <p>not found..</p>;
+};
+
+export default Custom404;
