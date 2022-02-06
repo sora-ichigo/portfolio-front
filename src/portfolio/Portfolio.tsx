95670e4876c01b33dce503d281791f8a36e7f3e9
igsr5 <i@gsr5.dev>
2022-02-06 18:05:28 +0900

    Add MediaItem

--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -1,37 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import { FaLeaf } from "react-icons/fa";
 
 import { GlobalCenterHeading } from "../common/components/GlobalCenterHeading";
 
 const CATEGORY = {
-  ABOUT_ME: 0,
-  RESUME: 1,
-  PORTFOLIO: 2,
-  CONTACT: 3,
+  CODING: 0,
+  DESIGN: 1,
+  LOGO: 2,
 } as const;
-type CategoryType = typeof CATEGORY[keyof typeof CATEGORY];
+type CategoryType = -1 | typeof CATEGORY[keyof typeof CATEGORY];
 
 const categoryData: { name: string; id: CategoryType }[] = [
   {
     name: "all",
-    id: 0,
+    id: -1,
   },
   {
     name: "coding",
-    id: 1,
+    id: CATEGORY["CODING"],
   },
   {
     name: "design",
-    id: 2,
+    id: CATEGORY["DESIGN"],
   },
   {
     name: "logo",
-    id: 3,
+    id: CATEGORY["LOGO"],
   },
 ];
 
+// TODO: テスト書く
+const GetCategoryName = (categoryID: CategoryType): string => {
+  let categoryName = "undefined";
+  categoryData.find((v) => {
+    if (v.id === categoryID) {
+      categoryName = v.name;
+    }
+  });
+
+  return categoryName;
+};
+
 export const Portfolio: React.FC = () => {
-  const [currentCategory, setCurrentCategory] = useState<CategoryType>(0);
+  const [currentCategory, setCurrentCategory] = useState<CategoryType>(-1);
 
   const CategoryBtn: React.FC<{ text: string; id: CategoryType }> = ({
     text,
@@ -63,6 +75,34 @@ export const Portfolio: React.FC = () => {
           ))}
         </ul>
       </div>
+
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <MediaItem
+          id={0}
+          title="personal cv project"
+          categoryID={CATEGORY["CODING"]}
+          imgUrl="https://themes.pixelwars.org/cvcard-wp/wp-content/uploads/2014/04/single-05.png"
+        />
+      </div>
     </>
   );
 };
+
+const MediaItem: React.FC<{
+  id: number;
+  title: string;
+  categoryID: CategoryType;
+  imgUrl: string;
+}> = ({ id, title, categoryID, imgUrl }) => (
+  <div className="mx-auto w-11/12 rounded border border-grey-4">
+    <a href={`#?${id}`}>
+      <img src={imgUrl} alt={title} />
+    </a>
+    <div className="px-10 py-7 text-center">
+      <h3 className="text-sm font-bold uppercase tracking-wider">{title}</h3>
+      <p className="uppercase text-grey-2" style={{ fontSize: "13px" }}>
+        {GetCategoryName(categoryID)}
+      </p>
+    </div>
+  </div>
+);
