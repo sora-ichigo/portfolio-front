b9ba4dfbe1e187107789ac11107ff10d1244a0d6
igsr5 <i@gsr5.dev>
2022-02-06 17:12:56 +0900

    Add CategoryBtn

--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaLeaf } from "react-icons/fa";
-import { Container } from "../common/components/Container";
+
+import { GlobalCenterHeading } from "../common/components/GlobalCenterHeading";
+
+const CATEGORY = {
+  ABOUT_ME: 0,
+  RESUME: 1,
+  PORTFOLIO: 2,
+  CONTACT: 3,
+} as const;
+type CategoryType = typeof CATEGORY[keyof typeof CATEGORY];
+
+const categoryData: { name: string; id: CategoryType }[] = [
+  {
+    name: "all",
+    id: 0,
+  },
+  {
+    name: "coding",
+    id: 1,
+  },
+  {
+    name: "design",
+    id: 2,
+  },
+  {
+    name: "logo",
+    id: 3,
+  },
+];
 
 export const Portfolio: React.FC = () => {
+  const [currentCategory, setCurrentCategory] = useState<CategoryType>(0);
+
+  const CategoryBtn: React.FC<{ text: string; id: CategoryType }> = ({
+    text,
+    id,
+  }) => (
+    <li
+      className={`${
+        id === currentCategory
+          ? "border-navy  bg-navy text-white hover:text-white"
+          : " border-grey-4  text-grey-2 hover:text-navy"
+      } my-2.5 mx-1.5 cursor-pointer rounded border px-3 py-1.5 text-xs
+       tracking-widest transition hover:border-navy`}
+      onClick={() => setCurrentCategory(id)}
+    >
+      {text}
+    </li>
+  );
+
   return (
-    <div className="my-3">
-      <div className="relative flex items-center justify-center">
-        <span className="absolute -z-10 w-full border-t-4 border-grey-1"></span>
-        <span className="flex items-center bg-white">
-          <div className="flex h-12 w-12 items-center justify-center rounded-full border-4 border-grey-1  text-grey-4">
-            <FaLeaf />
-          </div>
-          <span className="pl-3 pr-6 text-xs font-bold uppercase tracking-widest">
-            my works
-          </span>
-        </span>
+    <>
+      <div className="my-3">
+        <GlobalCenterHeading text="my works" icon={FaLeaf} />
       </div>
-    </div>
+
+      <div className="my-8 flex justify-center">
+        <ul className="flex uppercase">
+          {categoryData.map((v) => (
+            <CategoryBtn text={v.name} id={v.id} key={v.id} />
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
