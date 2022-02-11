b71dba2048d38afb45b44f3e1673b26191c07b65
igsr5 <i@gsr5.dev>
2022-02-11 23:23:47 +0900

    yarn add swiper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "tailwindcss/tailwind.css";
+import SwiperCore, { Pagination, Navigation } from "swiper";
 
 import { Footer } from "../common/components/Footer";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "tailwindcss/tailwind.css";
 import "../styles/index.css";
+
+SwiperCore.use([Pagination, Navigation]);
 
 const App = ({ Component, pageProps }: any) => {
   return (
