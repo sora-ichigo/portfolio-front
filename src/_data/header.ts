60342e4608a77a40f025c7cc3a4da4565b0bd76a
igsr5 <i@gsr5.dev>
2022-02-20 21:44:30 +0900

    Update Header

--- src/_data/header.ts
+++ src/_data/header.ts
@@ -3,4 +3,5 @@ import { HeaderDataType } from "../domain";
 export const headerData: HeaderDataType = {
   name: "Sora Ichigo",
   description: "software engineer",
+  profileImgUrl: "https://images.igsr5.com/l/profile.png",
 };
