8c544af1b1b3fed318c3f430dee080a5ebc55358
igsr5 <i@gsr5.dev>
2022-03-26 03:03:09 +0900

    Update _data/blog

--- src/_data/blog.ts
+++ src/_data/blog.ts
@@ -1,24 +1,5 @@
 import { BlogData, BlogItemType } from "../domain/blog";
 
-const serializeBlogItem = (
-  posts: {
-    [key: string]: string;
-  }[]
-): BlogItemType[] => {
-  let blogItems: BlogItemType[] = [];
-  posts.map((v, i) => {
-    blogItems.push({
-      id: i,
-      title: v.title,
-      thumbnailUrl: v.thumbnailUrl,
-      createdAt: new Date(v.createdAt),
-      link: v.link,
-    });
-  });
-
-  return blogItems;
-};
-
 export const blogData: BlogData = {
   blogItems: [] as BlogItemType[],
 };
