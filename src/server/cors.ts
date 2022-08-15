eadcb4a4a5a9a0065ddd39e555124dfbe871a9fc
igsr5 <igsr5.dev@gmail.com>
2022-08-16 00:22:24 +0900

    Update cors config

--- src/server/cors.ts
+++ src/server/cors.ts
@@ -4,6 +4,10 @@ import Cors from "cors";
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 export const corsConfig = Cors({
-  origin: ["http://localhost:3000", "https://igsr5.dev"],
+  origin: [
+    "http://localhost:3000",
+    "https://igsr5.dev",
+    new RegExp(/http:\/\/igsr5-front.*\.vercel.app/),
+  ],
   methods: ["POST", "GET", "HEAD"],
 });
