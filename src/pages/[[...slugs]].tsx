dc8780834241df09f8c7b1c84cbd538d3a95993c
igsr5 <igsr5.dev@gmail.com>
2022-08-17 22:40:56 +0900

    Migrate SSG

--- src/pages/[[...slugs]].tsx
+++ src/pages/[[...slugs]].tsx
@@ -1,8 +1,13 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { RootMain } from "../RootMain";
-import { MAIN_PAGES, MainPageType } from "../common/utils/mainPages";
+import {
+  MAIN_PAGES,
+  MainPageType,
+  mainPageList,
+} from "../common/utils/mainPages";
 import { axiosClient } from "../axios_client";
 import { Blog, BlogData } from "../domain";
+import { captureException } from "@sentry/nextjs";
 
 const Home: NextPage<{ pageType: MainPageType; blogData: BlogData }> = (
   props
@@ -12,34 +17,52 @@ const Home: NextPage<{ pageType: MainPageType; blogData: BlogData }> = (
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps<{
+export const getStaticProps: GetStaticProps<{
   pageType: MainPageType;
   blogData: BlogData;
 }> = async (ctx) => {
   let pageType: MainPageType = 0;
   let blogItems: Blog[] = [];
-  switch (ctx.req.url?.split("?")[0]) {
-    case "/":
+  let pageName: string | undefined;
+  if (Array.isArray(ctx.params?.slugs)) {
+    pageName = ctx.params?.slugs[0];
+  } else if (typeof ctx.params?.slugs === "string") {
+    pageName = ctx.params?.slugs;
+  }
+
+  switch (pageName) {
+    case "":
       pageType = MAIN_PAGES["ABOUT_ME"];
       break;
-    case "/resume":
+    case "resume":
       pageType = MAIN_PAGES["RESUME"];
       break;
-    case "/portfolio":
+    case "portfolio":
       pageType = MAIN_PAGES["PORTFOLIO"];
       break;
-    case "/blog":
+    case "blog":
       pageType = MAIN_PAGES["BLOG"];
       await axiosClient
         .get<{ blogs: Blog[] }>("/api/blogs")
         .then((res) => {
           blogItems = res.data.blogs;
         })
-        .catch((err) => {});
-      break;
-    default:
-      return { notFound: true };
+        .catch((err) => {
+          captureException(err);
+        });
   }
 
-  return { props: { pageType, blogData: { blogItems } } };
+  return {
+    props: { pageType, blogData: { blogItems } },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const allPaths = mainPageList.map((page) => {
+    return page.path;
+  });
+  return {
+    paths: allPaths,
+    fallback: true,
+  };
 };
