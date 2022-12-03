cd08f17f764c94ee7d3ef3360a59c8852ff7205c
igsr5 <igsr5.dev@gmail.com>
2022-12-03 14:11:52 +0900

    Refactoring _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import React, { ReactElement, ReactNode, useEffect, useState } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { AppProps } from "next/app";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 import SwiperCore, { Pagination, Navigation } from "swiper";
 import * as Sentry from "@sentry/react";
 
@@ -12,48 +11,23 @@ import "../styles/index.css";
 
 import { InternalServerError } from "../common/components/InternalServerError";
 import { GlobalHead } from "../common/components/GlobalHead";
-import { mainPageList, MAIN_PAGES } from "../common/utils/mainPages";
-
-SwiperCore.use([Pagination, Navigation]);
-
-const fallback = <InternalServerError></InternalServerError>;
 
 export type NextLayout = (page: ReactElement) => ReactNode;
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: NextLayout;
 };
-
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+SwiperCore.use([Pagination, Navigation]);
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
-  const router = useRouter();
-  const path = router.asPath;
-  const [currentPageString, setCurrentPageString] = useState("");
   const getLayout = Component.getLayout ?? ((page) => page);
-
-  useEffect(() => {
-    const aboutPage = mainPageList[MAIN_PAGES["ABOUT_ME"]];
-    if (path === aboutPage.path) {
-      setCurrentPageString(`${aboutPage.name[0].toUpperCase()}${aboutPage.name.slice(1)}`);
-      return;
-    }
-
-    for (const mainPage of mainPageList) {
-      if (mainPage === aboutPage) continue;
-
-      if (path.includes(mainPage.path)) {
-        setCurrentPageString(`${mainPage.name[0].toUpperCase()}${mainPage.name.slice(1)}`);
-        break;
-      }
-    }
-  }, [path]);
 
   return getLayout(
     <>
-      <GlobalHead currentPageString={currentPageString} />
-      <Sentry.ErrorBoundary fallback={fallback}>
+      <GlobalHead title="Sora Ichigo's HP" />
+      <Sentry.ErrorBoundary fallback={<InternalServerError />}>
         <Component {...pageProps} />
       </Sentry.ErrorBoundary>
     </>
